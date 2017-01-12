6f397483a02685c3eb0cd5ab66927dba0e8ec700
eyaleb <eyaleb@users.noreply.github.com>
2017-01-13 08:31:03 +1100

    Update i0.lua
    Remove stray duplicated line

--- examples/eyal/app_v3/i0.lua
+++ examples/eyal/app_v3/i0.lua
@@ -3,7 +3,6 @@ time_start = tmr.now()
 node.setcpufreq(node.CPU160MHZ)	-- do this asap
 time_dofile, start_dofile = 0, time_start
 if pcall (function() dofile("funcs.lc", true) end) then
-if pcall (function() dofile("funcs.lc", true) end) then
 	function Log (...) mLog ("init", unpack(arg)) end
 	local function Trace(n, new) mTrace(2, n, new) end Trace (0)
 	do_file ("main")
